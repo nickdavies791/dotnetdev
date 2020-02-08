66a146f381be790d851732e6a5beb07252dd73e8
Nick Davies <nick.davies@gentinguk.com>
2020-02-08 21:09:03 +0000

    If else statement with curly braces

--- BranchesAndLoops/Program.cs
+++ BranchesAndLoops/Program.cs
@@ -10,9 +10,13 @@ namespace BranchesAndLoops
             int b = 2;
 
             if (a + b > 10)
-                Console.WriteLine("The answer is greater than 10.");
+            {
+                Console.WriteLine("The answer is greater than 10.");                
+            }
             else 
+            {
                 Console.WriteLine("The answer is smaller than 10.");
+            }
         }
     }
 }
