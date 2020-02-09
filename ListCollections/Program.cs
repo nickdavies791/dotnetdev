fe5acf713045976713840a3a1eba2bd0468a99d8
Nick Davies <nick.davies@gentinguk.com>
2020-02-09 07:47:00 +0000

    Using item index and count property

--- ListCollections/Program.cs
+++ ListCollections/Program.cs
@@ -16,6 +16,10 @@ namespace ListCollections
             {
                 Console.WriteLine($"Hello, {name.ToUpper()}");
             }
+
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+            Console.WriteLine($"The list contains {names.Count} people.");
         }
     }
 }
