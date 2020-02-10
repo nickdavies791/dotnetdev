a0f55d36edba33be7952ade8688d26ae0f541a39
Nick Davies <nick.davies@gentinguk.com>
2020-02-10 09:47:27 +0000

    Create new class for BankAccount

---
+++ Classes/BankAccount.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; }
+        public demical Balance { get; }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            //
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            //
+        }
+    }
+}
