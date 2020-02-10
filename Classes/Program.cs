8637a5e513fc5df0c140fc10c8da1f3cf217159a
Nick Davies <nick.davies@gentinguk.com>
2020-02-10 11:05:05 +0000

    Catch exception thrown when an account is created with negative balance

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,20 +7,31 @@ namespace Classes
         static void Main(string[] args)
         {
             // Create a new account
-            var account = new BankAccount("Nick Davies", 2500);
-            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} opening balance.");
+            // var account = new BankAccount("Nick Davies", 2500);
+            // Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} opening balance.");
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance.");
+                Console.WriteLine(e.ToString());
+            }
 
             // Make a withdrawal
-            account.MakeWithdrawal(500, DateTime.Now, "Rent Payment");
-            Console.WriteLine($"Balance: {account.Balance}");
+            // account.MakeWithdrawal(500, DateTime.Now, "Rent Payment");
+            // Console.WriteLine($"Balance: {account.Balance}");
 
             // Make a deposit
-            account.MakeDeposit(2000, DateTime.Now, "Salary");
-            Console.WriteLine($"Balance: {account.Balance}");
+            // account.MakeDeposit(2000, DateTime.Now, "Salary");
+            // Console.WriteLine($"Balance: {account.Balance}");
 
             // Try to make a withdrawal over the available balance
-            account.MakeWithdrawal(7500, DateTime.Now, "Fraud");
-            Console.WriteLine($"Balance: {account.Balance}");
+            // account.MakeWithdrawal(7500, DateTime.Now, "Fraud");
+            // Console.WriteLine($"Balance: {account.Balance}");
+
         }
     }
 }
